2d7c0185a8b116efc4fbbc98fc55f3fc19fa2fb9
farkhan432 <achamdfarhan0101@gmail.com>
2021-02-23 17:01:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand " href="index.php"><strong><center>Stack <br> That Sale</center></strong></a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>    
+        
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-5 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="ourservices.php">Our Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#">Our project</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link ml-3 " href="#"   aria-expanded="false">
+              Blog
+            </a>
+           
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#" tabindex="-1" aria-disabled="true">Contact Us</a>
+          </li>
+        </ul>
+      
+      <button type="button" class="btn btn-danger btn-lg" href="#">Call us now</button>
+
+      <div class="gambarpojok">
+        <img src="img/jumbotron.jpg" style="width: 400px; height:400px;">
+      </div>
+
+      </div>
+      </div>
+     </nav>
+
+        <div class="jumbotron">
+            <h1 class="">Welcome Back </h1>
+            <h1>To Barbar E-sport Again</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling.</p>
+            <hr class="my-4">
+            <a class="btn2 btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a class="btn2 btn-light btn-lg" href="#" role="button">Learn more</a>
+        </div>
+
+
+        <div class="back">
+        <br>
+        <br>
+        <p class="lead text-center">
+        <b>OUR SERVICES</b> <br> WE become part of your team, meeting regulary to plant content, facilities business goals and get as a group
+        </p>
+
+          <div class="row">
+        <div class="card text-center">
+            <div class="card-header">
+              <span class="display-4">
+              <img src="img/komputer.jpg" class="2" alt="">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">somewhere</a>
+            </div>
+            </span>
+          </div>
+
+          <div class="card text-center">
+            <div class="card-header">
+              <span class="display-4">
+              <img src="img/pengeras.jpg" class="3" alt="">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </span>
+          </div>
+
+          <div class="card text-center">
+            <div class="card-header">
+              <span class="display-4">
+              <img src="img/analisis.jpg" classs="3" style="width:200px; height:180px;">
+            </div>
+            
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            </span>
+          </div>
+          </div>
+          </div>
+
+
+        <br>
+        <br>
+        <br>
+        <p class="our1" >
+        <b>OUR Client</b> <br>
+        
+        
+      
+        <div class="gambarpijik">
+        <img src="img/pijik.jpg" style="width: 400px; height:400px;">
+         <img src="img/logo1.jpg" class="logo1">
+         <img src="img/logo2.jpg" class="logo2">
+         <img src="img/logo3.jpg" class="logo3">
+         <img src="img/logo4.jpg" class="logo4">
+        
+         <img src="img/logo1.jpg" class="logo5">
+         <img src="img/logo2.jpg" class="logo6">
+         <img src="img/logo3.jpg" class="logo7">
+         <img src="img/logo4.jpg" class="logo8">
+         </div>
+         <a class="btn5 btn-danger btn-lg" href="#" role="button">Learn more</a>
+        </p>
+        
+        <br>
+        <br>
+        <br>
+        
+        <p class="our2" >
+        <b>Meet Our Team</b> <br>
+        
+
+        <div class="row">
+        <div class="card" style="width: 12rem;">
+          <img src="img/orang1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+          <p class="card-text">Clay Mashak <BR>Excutive Chairman</BR></p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 12rem; ">
+          <img src="img/orang2.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+          <p class="card-text">Gabriel Bailllio <br>Head of Marketing</p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 12rem; ">
+          <img src="img/orang3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+          <p class="card-text">Taylor Litzinger <br> Head of Design</p>
+          </div>
+        </div>
+
+        <div class="card" style="width: 12rem;">
+          <img src="img/orang4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+          <p class="card-text">Simonne Leever <br> Brand Strategy</p>
+          </div>
+        </div>
+        </div>
+
+        <br>
+        <br>
+        <br>
+        
+
+        <p class="our2" >
+        <div class="jumbotron">
+        <center>
+        <h1>Get In Touch</h1>
+        <br>
+        <h3>And Make Marketing 4.0 Perfomance your competince</h3>
+       <input type="text"  placeholder="Your name" id="fname" name="name">
+        <input type="text"  placeholder="Your email" id="fname" name="email">
+        <br><input type="text"  placeholder="Your email" id="fme" name="email">
+        <br>
+       <a class ="explore3" href="#">Send Message</a>   
+       </center> 
+       </div> 
+
+
+       <div class="end">
+         <h2 class=""><b>Stack <br>
+         ThatSale</b>
+         <br>
+         <br>
+         <h6>Smart Move to build your brand and engage <br>more customer</h6>
+         <br>
+         <br>
+         <img src="img/ig.jpg" id="sosmed">
+         <img src="img/fb.png" id="sosmed2">
+         <img src="img/twt.png" id="sosmed3">
+         <img src="img/in.jpg" id="sosmed4">
+         <h6 class="teks">Resource</h6>
+         <h6 class="teks2">Home</h6>
+         <h6 class="teks3">Service</h6>
+         <h6 class="teks4">Project</h6>
+         <h6 class="teks5">Blog</h6>
+         <h6 class="teks6">About Us</h6>
+         <h6 class="teks7">Services</h6>
+         <h6 class="teks8">Website & Mobile App Design</h6>
+         <h6 class="teks9">Digital Marketing Impelementation</h6>
+         <h6 class="teks10">Web Analytics Audit</h6>
+         <h6 class="teks11">Company</h6>
+         <h6 class="teks12">Add: 4666 Barrett Rd, Los Angeles,<br>
+         <br>
+          CA 90032</h6>
+          <h6 class="teks13">Tel: + 1 (800) 123 4567</h6>
+          <h6 class="teks14">Email: support@stackthatdeal.com</h6>
+         </div>  
+       </div>
+
+       <footer>
+          <div class="container">
+            <small>CopyRight &copy; 2021 - Tanpa Nama</small>
+          </div>
+       </footer>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>
---
+++ ourservices.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Hello, world!</title>
+  
+  </head>
+  <body>
+    <div class="kihiy">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container-fluid">
+    <a class="navbar-brand " href="index.php"><strong><center>Stack <br> That Sale</center></strong></a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>    
+        
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-5 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="ourservices.php">Our Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#">Our project</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link ml-3 " href="#"   aria-expanded="false">
+              Blog
+            </a>
+           
+          </li>
+          <li class="nav-item">
+            <a class="nav-link ml-3" href="#" tabindex="-1" aria-disabled="true">Contact Us</a>
+          </li>
+        </ul>
+      
+      <button type="button" class="btn btn-danger btn-lg" href="#">Call us now</button>
+
+      
+
+      </div>
+      </div>
+      </div>
+     </nav>
+     <div id="gambar">
+        <img src="img/pijik.jpg" >
+       <div class="box1">
+            <h1 class="">Right partner </h1>
+            <h1>Right Solution</h1>
+            <p class="">We intergrate, strategic planning. creative design and marketing ,public relations <br> media </p>
+            <hr class="my-8">
+            <a class="btn2 btn-primary btn-lg" href="#" role="button">Start your project</a>
+            <a class="btn2 btn-secondary btn-lg" href="#" role="button">Learn more</a>
+        
+            </div>
+      </div>
+
+        <div class="box2">
+            What we offer
+        </div>
+     
+      
+        <div class="jumbotron" id="yeah"> 
+            <img src="img/jumbotron.jpg" id="mobil1">
+        </div>
+
+        <div id="box2">   
+          <span>
+              <h1>oyy</h1>
+          </span>   
+        </div>
+
+        
+        <div class="jumbotron" id="yeah2">
+            <h1 class="">Welcome Back </h1>
+            <h1>To Barbar E-sport Again</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling.</p>
+            <hr class="my-4">
+            <a class="btn2 btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <a class="btn2 btn-light btn-lg" href="#" role="button">Learn more</a>
+        </div>
+
+        <div id="box3"> 
+          <span>
+            
+          </span>     
+        </div>
+
+        <div class="box3">
+         <h2 id="itu">Stack 
+           <h2 id="itu2">ThatSale  
+            <h2 id="itu3"> Smart Move to build your brand and engage
+                        <img src="img/ig.jpg" id="sosmed5">
+                      <img src="img/fb.png" id="sosmed6">
+                      <img src="img/twt.png" id="sosmed7">
+                      <img src="img/in.jpg" id="sosmed8">
+                      
+            </h2>
+            </h2>
+         </h2>
+        </div>
+
+
+
+
+
+
+
+
+      <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
